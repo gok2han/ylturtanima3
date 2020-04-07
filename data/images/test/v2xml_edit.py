a7cf0250942c2bdbd632eb536be4541427b57e64
Gökhan <gokgokhan@gmail.com>
2020-04-07 21:09:38 +0300

    V3

---
+++ data/images/test/v2xml_edit.py
@@ -0,0 +1,13 @@
+import glob
+from xml.etree import ElementTree as et
+
+prefix = 'C:\\Users\\Gokhan GOK\\Google Drive\\YlisansBiyoloji\\01Cetoniinae\\01_Cetonia_aurata\\'
+
+for entry in glob.glob('*.xml'):
+    print(entry)
+    tree = et.parse(entry)
+    print(tree.find('path').text)
+    tree.find('path').text = prefix + entry
+    tree.find('filename').text = entry.replace("xml","jpg")
+    tree.write(entry)
+
