082577e7237fbdbb40ed0a3c290d1d0f0b044db6
Gökhan <gokgokhan@gmail.com>
2020-04-07 23:47:29 +0300

    v4

--- data/images/test/v2xml_edit.py
+++ data/images/test/v2xml_edit.py
@@ -3,7 +3,7 @@ from xml.etree import ElementTree as et
 
 prefix = 'C:\\Users\\Gokhan GOK\\Google Drive\\YlisansBiyoloji\\01Cetoniinae\\01_Cetonia_aurata\\'
 
-for entry in glob.glob('*.xml'):
+for entry in glob.glob('*.jpg'):
     print(entry)
     tree = et.parse(entry)
     print(tree.find('path').text)
